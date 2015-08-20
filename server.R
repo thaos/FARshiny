dc4713f3353a2d4a7893c3c941bf817b8c22d676
thaos <kajhiua@hotmail.com>
2015-08-20 18:30:05 +0200

    back to no tabs

--- server.R
+++ server.R
@@ -14,7 +14,31 @@ shinyServer(function(input, output) {
               })
 
               data_loaded <- reactive({
-                is.null(read_data())
+                !is.null(read_data())
+              })
+
+              output$b1 <- renderUI({
+                if(!data_loaded()) return()
+                actionButton("b1", "Plot Data")
+              })
+
+              output$fit_buttons <- renderUI({
+                if(is.null(input$b1)) return()
+                if(input$b1 == 0) return()
+                list(
+                     selectInput("fit_method", "Choose a fitting method:", 
+                            choices = c("Gaussian", "GEV", "GPD")),
+                     uiOutput("threshold"),
+                     br(),
+                     actionButton("b2", "Fit Model")
+                     )
+              })
+              output$threshold <- renderUI({
+                if(is.null(input$fit_method)) return()
+                if(input$fit_method == "GPD"){
+                  sliderInput("threshold", "Select GPD threshold", min=0, max=1, value=0.9, step=0.005)
+                }
+                else return()
               })
 
               output$contents <- renderTable({
@@ -30,17 +54,8 @@ shinyServer(function(input, output) {
               })
 
               plot_data <- reactive({
-                ydat <- read_data()
-                ydat <- ydat[order(ydat$year),]
-                plot(ydat$year, ydat$y, ylab="y", xlab="years")
-                if(!is.null(input$fit_method)) {
-                  if(input$fit_method == "GPD") {
-                    lines(ydat$year, predict(quantreg::rq(y~mu_var, data=ydat, tau=input$threshold)), col="red", lwd=2)
-                  }
-                }
-              })
-
-              plot_data_ic <- reactive({
+                if(is.null(input$b1)) return()
+                if(input$b1 == 0) return()
                 ydat <- read_data()
                 ydat <- ydat[order(ydat$year),]
                 plot(ydat$year, ydat$y, ylab="y", xlab="years")
@@ -51,6 +66,7 @@ shinyServer(function(input, output) {
                 }
                 if(!is.null(input$ic_method)) {
                   if(!is.null(input$ic_method)) {
+                    print("j'suis")
                     abline(h=input$xp, col="blue", lwd=2)
                     abline(v=input$t0t1[1], col="blue", lwd=2)
                     abline(v=input$t0t1[2], col="blue", lwd=2)
@@ -60,34 +76,20 @@ shinyServer(function(input, output) {
 
               output$data_plot1 <- renderPlot({
                 print("plotting data for fit")
-                input$fit_method
                 plot_data()
               }, width=600, height=400)
-
-              output$data_plot2 <- renderPlot({
-                print("plotting data for ic")
-                plot_data_ic()
-              }, width=600, height=400)
               
-              output$threshold <- renderUI({
-                if(is.null(input$fit_method)) return()
-                if(input$fit_method == "GPD"){
-                  sliderInput("threshold", "Select GPD threshold", min=0, max=1, value=0.9, step=0.005)
-                }
-                else return()
-              })
-
-              # Return the requested dataset
+              #Return the requested dataset
               fit_input <- reactive({
                 print("modifying fit method")
                 if(is.null(input$fit_method)) return()
                 switch(input$fit_method,
-                                     "Gaussian" = gauss_fit,
-                                     "GEV" = gev_fit,
-                                     "GPD" = function(x)gpd_fit(x, qthreshold=input$threshold)
-                                     )
+                       "Gaussian" = gauss_fit,
+                       "GEV" = gev_fit,
+                       "GPD" = function(x)gpd_fit(x, qthreshold=input$threshold)
+                       )
               })
-              
+
               fit_model  <- reactive({
                 print("fitting model")
                 fit_input()(read_data())
@@ -98,49 +100,46 @@ shinyServer(function(input, output) {
                 input$b2
                 isolate({plot(fit_model())})
               })
+              output$select_ic <- renderUI({
+                if(is.null(input$b2)) return()
+                if(input$b2 == 0) return()
+                ydat <- read_data()
+                y_fit <- fit_input()(ydat)
+                list(
+                     selectInput("ic_method", "Choose a method for CI",
+                                 choices = c("Profile", "Bootstrap")),
+                     sliderInput("xp", "Select Event Threhsold", min=min(ydat$y), max=max(ydat$y), value=median(ydat$y)),
+                     sliderInput("t0t1", "Select Starting and Ending Dates", min=min(ydat$year), max=max(ydat$year), step=1,value=range(ydat$year)),
+                     actionButton("b3", "Compute CI")
+                     )
+              })
 
+              ic_input <- reactive({
+                switch(input$ic_method,
+                       "Profile" = prof_ic,
+                       "Bootstrap" = boot_ic 
+                       )
+              })
+
+              output$compute_ic <- renderPrint({
+                if(is.null(input$b3))return()
+                if(input$b3 == 0) return()
+                input$b3
+                isolate({
+                  ydat <- read_data()
+                  if( input$ic_method == "Bootstrap" & input$fit_method == "GPD")
+                    ic_func <- function(xp, t0, t1, y_fit, ci_p=0.95) ic_input()(xp, t0, t1, y_fit, ci_p=ci_p ,under_threshold=TRUE)
+                  else 
+                    ic_func <- ic_input()
+                  y_fit <- fit_input()(ydat)
+                  log <- capture.output({
+                    ic_fit <- ic_func(xp=input$xp, t0=input$t0t1[1], t1=input$t0t1[2], y_fit=y_fit)
+                  })
+                  return(ic_fit)
+                })
+              })
+              # 
+              
 })
 
-# 
-# 
-# 
-# 
-# 
-              #               output$select_ic <- renderUI({
-              #                 if(is.null(input$b2)) return()
-              #                 if(input$b2 == 0) return()
-              #                 ydat <- read_data()
-              #                 y_fit <- fit_input()(ydat)
-              #                 list(
-              #                      selectInput("ic_method", "Choose a method for CI",
-              #                      choices = c("Profile", "Bootstrap")),
-              #                      sliderInput("xp", "Select Event Threhsold", min=min(ydat$y), max=max(ydat$y), value=median(ydat$y)),
-              #                      sliderInput("t0t1", "Select Starting and Ending Dates", min=min(ydat$year), max=max(ydat$year), step=1,value=range(ydat$year)),
-              #                      actionButton("b3", "Compute CI")
-              #                      )
-              #               })
-              # 
-              #               ic_input <- reactive({
-              #                 switch(input$ic_method,
-              #                        "Profile" = prof_ic,
-              #                        "Bootstrap" = boot_ic 
-              #                        )
-              #               })
-              #               
-              #               output$compute_ic <- renderPrint({
-              #                 if(is.null(input$b3))return()
-              #                 if(input$b3 == 0) return()
-              #                 input$b3
-              #                 isolate({
-              #                   ydat <- read_data()
-              #                   if( input$ic_method == "Bootstrap" & input$fit_method == "GPD")
-              #                     ic_func <- function(xp, t0, t1, y_fit, ci_p=0.95) ic_input()(xp, t0, t1, y_fit, ci_p=ci_p ,under_threshold=TRUE)
-              #                   else 
-              #                     ic_func <- ic_input()
-              #                   y_fit <- fit_input()(ydat)
-              #                   log <- capture.output({
-              #                     ic_fit <- ic_func(xp=input$xp, t0=input$t0t1[1], t1=input$t0t1[2], y_fit=y_fit)
-              #                   })
-              #                   return(ic_fit)
-              #                 })
-              #               })
+
--- ui.R
+++ ui.R
@@ -3,8 +3,6 @@ shinyUI(
         pageWithSidebar(
                         headerPanel("FAR Analysis"),
                         sidebarPanel(
-                                     #                                      conditionalPanel(
-                                     #                                                       "$('li.active a').first().html()==='Data'",
                                                       fileInput('file1', 'Choose CSV File',
                                                                 accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
                                                       checkboxInput('header', 'Header', TRUE),
@@ -20,36 +18,20 @@ shinyUI(
                                                                      'Single Quote'="'"),
                                                                    'Double Quote'),
                                                       numericInput("obs", "Number of observations to view:", 10),
-                                                      #                                                       ),
-                                                      #                                      conditionalPanel(
-                                                      #                                                       "$('li.active a').first().html()==='Model Fit'",
-                                                      selectInput("fit_method", "Choose a fitting method:", 
-                                                                         choices = c("Gaussian", "GEV", "GPD")),
-                                                      uiOutput("threshold"),
+                                                      uiOutput("b1"),
                                                       br(),
-                                                      actionButton("b2", "Fit Model"),
-                                                      #                                                       ),
-                                                      #                                      conditionalPanel(
-                                                      #                                                       "$('li.active a').first().html()==='FAR Estimation'",
+                                                      uiOutput("fit_buttons"),
+                                                      br(),
                                                       uiOutput("select_ic")
-                                                      #                                                       )
                                      ),
                         mainPanel(
-                                  #                                   tabsetPanel(
-                                  #                                               tabPanel("Data", 
                                                        verbatimTextOutput("summary"), 
                                                        tableOutput("contents"),
-                                                       #                                                        ),
-                                                       #                                               tabPanel("Model Fit", 
                                                        plotOutput("data_plot1"),
                                                        plotOutput("fit_plot"),
-                                                       #                                                        ),
-                                                       #                                               tabPanel("FAR Estimation", 
                                                        plotOutput("data_plot2"),
                                                        verbatimTextOutput("compute_ic")
                                                        )
-                        #                                               )
-                        #                                   )
                         )
         )
                                                                     
